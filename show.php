9465398e4ec55701fcd8d0f7822b12bd137f58ef
ton <tonzab84@hotmail.com>
2018-04-03 11:05:18 +0700

    โชว์
    แสดงข้อมูล

---
+++ show.php
@@ -0,0 +1,12 @@
+<?php
+	$age = 20;
+	$myname = 'Talon';
+	$product_price = 30.25;
+	$mystatus = true;
+	$mysurname = null;
+	
+	echo "ชื่อของฉันคือ $myname<br>";
+	echo "อายุของฉันคือ $age<br>";
+	echo 'ราคาสินค้า = '.$product_price;
+	
+
